a424a610d8615c43d405122b083ed5ceff3d160c
Md. Tarikul Islam <tarikulwebx@gmail.com>
2023-11-27 10:25:27 +0600

    Initial setup

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+}
